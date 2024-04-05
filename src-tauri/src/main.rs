542366385ae1a7292eb83714f5b7a8901636e86c
Nipe Setiawan Harefa <nipeharefa@gmail.com>
2024-04-05 14:45:48 +0700

    ci

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,14 +1,28 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::{Manager};
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
+fn greet(name: &str, window: tauri::Window) -> String {
+    println!("Window: {}", window.available_monitors().unwrap().len());
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
 fn main() {
+    
     tauri::Builder::default()
+        .setup(|app| {
+            // let main_window = app.get_window("main").unwrap();
+            let window = app.get_window("main").unwrap();
+            std::thread::spawn(move || {
+                println!("This will print!");
+                let _monitors = window.available_monitors().unwrap();
+                println!("This will print! ${}", _monitors.len());
+            });
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
